ca4089cc5a6639a51297b6f5af8f457bd6f556b6
George Lei <george.lei@lesmills.com>
2024-03-28 20:59:13 +1300

    Add the resolver function of Query.getUser

--- src/functions/appsync-query-resolver/__tests__/unit/index.test.ts
+++ src/functions/appsync-query-resolver/__tests__/unit/index.test.ts
@@ -1,9 +1,13 @@
 import { handler } from "../..";
 
 describe("handler", () => {
-  it("should log the event", () => {
-    console.log = jest.fn();
-    handler({ arguments: {}, source: { some: "event" } } as any);
-    expect(console.log).toHaveBeenCalled();
+  it("should log the event", async () => {
+    const event = { arguments: { id: "123456789" } };
+    const user = await handler(event as any);
+    expect(user).toEqual({
+      id: "123456789",
+      name: "John Doe",
+      email: "example@email.com",
+    });
   });
 });
--- src/functions/appsync-query-resolver/index.ts
+++ src/functions/appsync-query-resolver/index.ts
@@ -1,5 +1,19 @@
 import { AppSyncResolverEvent } from "aws-lambda";
+import { User } from "../../schemas/user";
 
-export async function handler(event: AppSyncResolverEvent<{}, {}>) {
+type Arguments = {
+  id: string;
+};
+
+// Handle getUser query
+export async function handler(
+  event: AppSyncResolverEvent<Arguments>
+): Promise<User> {
   console.log("Received event:", JSON.stringify(event, null, 2));
+
+  return {
+    id: "123456789",
+    name: "John Doe",
+    email: "example@email.com",
+  };
 }
