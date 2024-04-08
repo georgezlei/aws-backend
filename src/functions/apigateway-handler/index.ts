7c4ab6b15f94bcac760f61735de0d55deaf97c7c
George Lei <george.lei@lesmills.com>
2024-04-08 21:52:02 +1200

    Update API Gateway handler

--- src/functions/apigateway-handler/index.ts
+++ src/functions/apigateway-handler/index.ts
@@ -1,3 +1,14 @@
-export function handler(event: any) {
+import { APIGatewayProxyEventV2, APIGatewayProxyResultV2 } from "aws-lambda";
+
+export function handler(
+  event: APIGatewayProxyEventV2
+): APIGatewayProxyResultV2 {
   console.log("Received event:", JSON.stringify(event, null, 2));
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      name: "John Doe",
+      email: "john@email.com",
+    }),
+  };
 }
